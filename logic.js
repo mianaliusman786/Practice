53b026074ee4b946b35d78ad9c446570b3cbb11c
mianaliusman786 <mianaliusman95@gmail.com>
2024-09-05 10:21:51 +0500

    asyncfuntion

--- logic.js
+++ logic.js
@@ -1,13 +1,19 @@
-function getData(dataid, getNextdata) {  // Line 1
-    setTimeout(() => {                   // Line 2
-        console.log("data", dataid);      // Line 3
-        if (getNextdata) {                // Line 4
-            getNextdata();                // Line 5 (Call callback once)
-        }
-    }, 2000);                             // Line 6
+function asyncfunction () {
+    return new Promise((resolve, reject) => {
+        setTimeout
+        (()=>{
+            console.log("some data1")
+            resolve(success);
+
+        }, 4000);
+    });
+    
 }
 
-getData(1, () => {                        // Line 7
-    getData(2);                           // Line 8
-    getData(7);                           // Line 9 (Call getData with dataid 7)
-});
+console.log("fetching data1");
+let p1 =asyncfunction();
+p1.then((res) =>{
+    console.log(res);
+}
+
+);
