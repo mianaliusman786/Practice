cbb37fa28a5f9472acb4453218bc6c2103230ad1
mianaliusman786 <mianaliusman95@gmail.com>
2024-09-05 09:31:36 +0500

    update

--- logic.js
+++ logic.js
@@ -1,6 +1,13 @@
-console.log("ali");
-console.log("mian");
+function getData(dataid, getNextdata) {  // Line 1
+    setTimeout(() => {                   // Line 2
+        console.log("data", dataid);      // Line 3
+        if (getNextdata) {                // Line 4
+            getNextdata();                // Line 5 (Call callback once)
+        }
+    }, 2000);                             // Line 6
+}
 
-setTimeout(() => {
-    console.log("usman")
-}, 6000);
+getData(1, () => {                        // Line 7
+    getData(2);                           // Line 8
+    getData(7);                           // Line 9 (Call getData with dataid 7)
+});
